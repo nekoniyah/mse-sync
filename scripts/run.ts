9e770828adc1badce9901c1d8a374d5b211a3efd
nekoniyah <nekomancers0@gmail.com>
2025-06-12 13:43:50 +0200

    &a

--- scripts/run.ts
+++ scripts/run.ts
@@ -6,4 +6,4 @@ const portFilePath = path.join(__dirname, "..", "config", "port.txt");
 
 const port = fs.readFileSync(portFilePath, "utf-8");
 
-app.listen(port);
+app.listen(parseInt(port), "0.0.0.0");
